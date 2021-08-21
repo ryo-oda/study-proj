28510d80b8aaaf434b9f9530368edc31ec9201d9
ryo-oda <saadiq1313@gmail.com>
2021-08-22 08:03:19 +0900

    ruby_1

---
+++ study-proj/ruby/HackerRank/method.rb
@@ -0,0 +1,6 @@
+def odd_or_even(number)
+    return number.even?
+end
+
+print odd_or_even(1)
+print odd_or_even(2)
---
+++ study-proj/ruby/HackerRank/method_param.rb
@@ -0,0 +1,9 @@
+def check_range(a, b, c)
+    return a.range?(b, c)
+end
+
+//うごかん　
+
+puts check_range(1, 2, 3)
+
+puts check_range(2, 1, 3)
